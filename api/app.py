95f70e28cbcfae5d54de6bb056405c834ec72ca4
xiebing <xiebing@zhejianglab.com>
2023-03-09 19:13:01 +0800

    feat: 修改uvicorn来开启https

--- api/app.py
+++ api/app.py
@@ -37,4 +37,5 @@ async def cc(message: Message, Authorization: Union[str, None] = Header(default=
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run("app:app", host="0.0.0.0", port=8000)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True,
+                ssl_keyfile='./key.pem', ssl_certfile='./cert.pem')
